0b0c82c09627bf59d4bebbd1edfc2588f35e1465
AmirhosseiniS <saharamh21@gmail.com>
2025-09-23 22:16:36 +0330

    Add files via upload

---
+++ Phonebook Database.sql
@@ -0,0 +1,7 @@
+CREATE TABLE contacts (
+  `number` INT NOT NULL,
+  `first_name` VARCHAR(45) NOT NULL,
+  `last_name` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`number`));
+
